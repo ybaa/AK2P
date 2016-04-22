dd0332e89e0044052953c3bc00f82386c5178229
Martyna Łagożna <m.lagozna@live.com>
2016-04-22 04:25:36 +0200

    naprawiony program, dzialają przyciski do ustawiania minut i godzin

--- main.c
+++ main.c
@@ -1,93 +1,113 @@
 /*
- * GccApplication1.c
+ * GccApplication5.c
  *
- * Created: 31.03.2016 16:01:36
- * Author : Martyna
+ * Created: 22.04.2016 00:36:04
+ * Author : Martyna;
  */ 
+
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/sfr_defs.h>
 
-void mk_delay_ms(uint16_t ms);
-uint8_t hour=0;
+//----------------------------zmienne------------------------------------------------------------------------;
+uint8_t hour = 0, sec = 0, min = 0;
+uint8_t wasThere = 0, helpHour = 0;
 int hoursTab[] = {0,1,2,3,4,5,6,7,64,65,66,67,68,69,70,71,128,129,130,131,132,133,134,135};
-	
-int main(void)
-{
-	DDRC = 0xFF;
-	PORTC = 0x00;
-	DDRD = 0xFF;
-	PORTD = 0x00;
-	//DDRB = 0xFF; //obsluga guzika nie dzialala, bo kazdy pin byl ustawiony na output
-	DDRB = 0b11001111; 
-	PORTB = 0x00;
-	PORTB |= (1<<PB4);
-	PORTB |= (1<<PB5);
 
-	uint8_t sec = 0, min = 0;//hour = 0;
-	
 
-    /* Replace with your application code */
+
+
+int main(void){
+	
+//------------------------porty, piny, ddry------------------------------------------------------------------;
+DDRC = 0xFF;					// wszystkie piny s¹ outputami -> diody;
+PORTC = 0x00;
+
+DDRD = 0xFF;					//wszystkie piny s¹ outputami -> diody;
+PORTD = 0x00;
+
+DDRB = 0b11001111;				//2 piny to przyciski, wiec sa ustawione na inputy;
+PORTB = 0b00110000;
+	
+	
+	
+    _delay_ms(3000);
+	
+	
     while (1) {
 		
-		PORTB = hoursTab[hour];
-		PORTD = min;
-		
-		for(sec = 0; sec < 10; sec++){
-
+		//--------------------------------petla dla sekund------------------------------------------------------------------;
+		for ( sec = 0; sec < 60; sec ++){
 			PORTC = sec;
-				
-			if(!(PINB & (1<<PB5)))
-			{
-				sec=0;
-				hour++;
-				PORTB = hoursTab[hour];
-				PORTC = sec;
-			}
 			
-			if(!(PINB & (1<<PB4)))
-			{
-				sec=0;
-				min++;
+			//---------------------------------ustawianie minut-------------------------------------------------------------;
+			//while(!(PINB & 0x10)){
+			while(!(bit_is_set(PINB, PINB4))){
+				PORTB = 0b00010000;				//atmega nie ogarnia bez tego przy probie ustawiania godzin po minutach gdzie ma outpty a gdzie inputy...;
+				sec = 0;						// do czasu az wcisniety jest przycisk, sekudny sie zatrzymuja, a minuty rosna co sekunda;
+				min++;							
+				if(min == 60)
+					min = 0;
 				PORTD = min;
 				PORTC = sec;
+				_delay_ms(1000);
+			}
+			PORTB = hoursTab[hour];				//po ustawieniu mu w petli wyzej gdzie ma outputy trzeba wrocic do ustawionej godziny;
+		
+			//--------------------------------ustawianie godzin--------------------------------------------------------------;
+		
+		
+				while(!(bit_is_set(PINB, PINB5))){			//warunek dziala -> sukces;
+					if(wasThere == 0){
+						PORTC = 255;						//niech zamiga wszystkimi diodami, jako ze jest w trybie ustawiania godzin;
+						_delay_ms(500);
+						PORTB = 0b00110000;					//trzeba to ustawic, zeby atmega ogarnela, ze gdzie ma outputy a gdzie inputy;
+					}
+					
+					wasThere = 1;							//godziny beda sie pogladowo ustawiac na sekundach, a nastepnie przenosic w miejsce diod dla godzin;
+				sec = 0;
+				helpHour++;
+				if(helpHour == 24)
+				helpHour = 0;
+				PORTC = helpHour;
+				_delay_ms(1000);
+				
+				
+			}
+		
+			if(wasThere == 1){								//porzadek, przeniesienie wyswietlania godizny na poprawne miejsce;
+			hour = helpHour;
+			PORTB = hoursTab[hour];
+			wasThere = 0;
+			helpHour = 0;
 			}
 			
 			_delay_ms(1000);
-			//mk_delay_ms(1000);
-			 		
 		}
 		
+		
+		//inkrementuje godzine, kiedy ma 60min;
 		min++;
-		if(min == 10){
+		if(min == 60){
 			hour++;
-			PORTB = hour;
 			min = 0;
-		}
-		
-	
-		if(hoursTab[hour] > 135){
-			sec = 0; 
-			min = 0;
-			hour = 0;
-		}
-		
-		
-    }
-}
-
-
-
-/*void mk_delay_ms(uint16_t ms)
-{
-	while(ms=-500)
-	{
-		_delay_ms(500);
-		
-		if(!(PINB & (1<<PB4)))
-		{
-			hour++;
+			sec = 0;
 			PORTB = hoursTab[hour];
 		}
-	}
-}*/
+		
+		//polnoc, a wiec wyzerowanie;
+		if(hoursTab[hour] > 135){
+			min = 0;
+			hour = 0;
+			sec = 0;
+			PORTB = hoursTab[hour];
+		}
+		
+		PORTD = min;
+
+	
+    }
+
+}
+
